090d994b9611cfec5eed03d0f3d3d744b0629261
ashutosh-engineer <ashutoshsingh6376@gmail.com>
2026-07-16 09:36:02 +0530

    Added

---
+++ cf-112A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    // Convert both strings to lowercase
+    for (char &c : s1) c = tolower(c);
+    for (char &c : s2) c = tolower(c);
+
+    if (s1 < s2) cout << -1;
+    else if (s1 > s2) cout << 1;
+    else cout << 0;
+
+    return 0;
+}
